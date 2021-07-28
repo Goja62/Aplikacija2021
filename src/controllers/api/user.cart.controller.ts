585de77d0a0b7e2025e78962c51e6144a0f29fb1
Goja <gortnar@beograd.com>
2021-07-28 17:24:31 +0200

    Mehanizam za slanje porudžbine 28.07.2021

--- src/controllers/api/user.cart.controller.ts
+++ src/controllers/api/user.cart.controller.ts
@@ -3,14 +3,18 @@ import { Request } from "express";
 import { AddArticleToCartDto } from "src/dtos/cart/add.article.to.cart.dto";
 import { EditArticleIoCartDto } from "src/dtos/cart/edit.article.in.cart.dto";
 import { Cart } from "src/entities/cart.entity";
+import { Order } from "src/entities/order.entity";
 import { AllowToRoles } from "src/misc/allow.to.roles.descriptor";
+import { ApiResponse } from "src/misc/api.response";
 import { RoleCheckedGuard } from "src/misc/role.checked.guard";
 import { CartService } from "src/services/cart/cart.service";
+import { OrderService } from "src/services/order/order.service";
 
 @Controller('api/user/cart')
 export class UserCartController {
     constructor(
         private cartService: CartService,
+        private orderService: OrderService,
     ) { }
 
     private async getActiveCartForUserId(userId: number): Promise<Cart> {
@@ -46,5 +50,13 @@ export class UserCartController {
     async changeQuantity(@Body() data: EditArticleIoCartDto, @Req() req: Request): Promise<Cart> {
         const cart = await this.getActiveCartForUserId(req.token.id);
         return await this.cartService.changeQuantity(cart.cartId, data.articleId, data.quantity)
+    }
+
+    @Post('makeOrder') // POST http://localhost:3000/api/user/cart/makeOrder/
+    @UseGuards(RoleCheckedGuard)
+    @AllowToRoles('user')
+    async makeOrder(@Req() req: Request): Promise<Order | ApiResponse> {
+        const cart = await this.getActiveCartForUserId(req.token.id);
+        return await this.orderService.add(cart.cartId);
     }
 }
